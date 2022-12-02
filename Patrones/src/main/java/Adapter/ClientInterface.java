20a47e0921009dba48b6a17ca22a38c11c42315a
Juan Pablo Plúas <107544861+jppluas@users.noreply.github.com>
2022-12-01 21:54:37 -0500

    a

---
+++ Patrones/src/main/java/Adapter/ClientInterface.java
@@ -0,0 +1,9 @@
+
+package Adapter;
+
+public interface ClientInterface {
+    
+    public void consultarSaldo();
+    public void pedirCredito();
+    public void obtenerHistorialCrediticio();
+}
---
+++ Patrones/src/main/java/Adapter/MobileAdapter.java
@@ -0,0 +1,21 @@
+
+package Adapter;
+
+public class MobileAdapter implements ClientInterface {
+
+    @Override
+    public void consultarSaldo() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public void pedirCredito() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public void obtenerHistorialCrediticio() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+    
+}
